faa174f3c3986440ff36a3fd04b70c615e8d005c
Andres Mauricio Garcia Caicedo <150713356+MauroGarcia-97@users.noreply.github.com>
2023-11-15 23:58:47 -0500

    Add files via upload

---
+++ Java/FizzBuzz/src/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,36 @@
+package fizzbuzz;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Ingrese el numero: ");
+            
+            int n = sc.nextInt();
+            System.out.println(" Los numeros de Fizz, Buzz y FizzBuzz son: ");
+            for (int i = 1; i <= n; i++)
+            {
+                if (i%3==0 && i%5==0)
+                {
+                    System.out.print("FizzBuzz");
+                }
+                else if (i%3==0)
+                {
+                    System.out.print("Fizz");
+                }
+                else if (i%5==0)
+                {
+                    System.out.print("Buzz");
+                }
+                else
+                {
+                    System.out.print(i);
+                }
+                System.out.print(","+" ");
+            }
+        } 
+    }
+    
+}
