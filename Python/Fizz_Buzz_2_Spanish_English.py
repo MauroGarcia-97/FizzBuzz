3f111243efbdcd596d6cf64ae75f3c33e87cc7b7
Andres Mauricio Garcia Caicedo <150713356+MauroGarcia-97@users.noreply.github.com>
2023-11-20 17:47:35 -0500

    Update and rename Fizz_Buzz_2.py to Fizz_Buzz_2_Spanish_English.py

--- Python/Fizz_Buzz_2_Spanish_English.py
+++ Python/Fizz_Buzz_2_Spanish_English.py
@@ -1,5 +1,6 @@
 #   2º Forma larga Utilizando Ciclos & Condicionales 
-for num in range(1,101):
+#   2º Long Form Using Loops & Conditionals.
+for num in range(1,101):s
     string = ""
     if num % 3 == 0:
         string = string + "Fizz"
