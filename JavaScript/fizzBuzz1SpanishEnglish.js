7c6a826f46cbb666a4126a82fe8324f9ab5dbd7c
Andres Mauricio Garcia Caicedo <150713356+MauroGarcia-97@users.noreply.github.com>
2023-11-20 18:17:01 -0500

    Update and rename fizzBuzz1.js to fizzBuzz1SpanishEnglish.js

--- JavaScript/fizzBuzz1SpanishEnglish.js
+++ JavaScript/fizzBuzz1SpanishEnglish.js
@@ -1,4 +1,5 @@
 /*   1º Forma Corta Utilizando Funcion Corta llamada Ternario */
+/*   1º Short Way Using ternary function */
 for (let i = 0; i < 100;) {
     console.log((++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i);
   }
