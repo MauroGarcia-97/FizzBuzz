68aad3305c8f650adbcd3548ddb9fc6cbad6f3f9
Andres Mauricio Garcia Caicedo <150713356+MauroGarcia-97@users.noreply.github.com>
2023-11-20 17:44:31 -0500

    Update and rename Fizz_Buzz_1.py to Fizz_Buzz_1_ Spanish_English.py

--- Python/Fizz_Buzz_1_ Spanish_English.py
+++ Python/Fizz_Buzz_1_ Spanish_English.py
@@ -1,4 +1,5 @@
 #   1º Forma Corta Utilizando Funcion Corta Lambda 
+#   1º Short Way Using  Lambda function
 print(*map(lambda i: 'Fizz'*(not i%3)+'Buzz'*(not i%5) or i, range(1,101)),sep='\n')
 
 
