8a99bc7cba2c56deead860adc863ecc9133bbdd0
Andres Mauricio Garcia Caicedo <150713356+MauroGarcia-97@users.noreply.github.com>
2023-11-20 18:18:11 -0500

    Update and rename fizzBuzz2.js to fizzBuzz2SpanishEngIish.js.js

--- JavaScript/fizzBuzz2SpanishEngIish.js.js
+++ JavaScript/fizzBuzz2SpanishEngIish.js.js
@@ -1,4 +1,5 @@
 /*   2º Forma larga Utilizando Ciclos & Condicionales */
+/*   2º Long Form Using Loops & Conditionals.         */
 for (var i = 1; i < 101; i++) {
     if (i % 15 == 0) console.log("FizzBuzz");
     else if (i % 3 == 0) console.log("Fizz");
